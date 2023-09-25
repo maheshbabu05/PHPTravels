c66a806f91576614d2217c09177dae51ad2c0d78
Mahesh Babu <Mahesh Babu@MaheshBoard>
2023-09-25 21:48:50 +0530

    Second Commit

---
+++ src/test/java/Page_Objects/SignUp.java
@@ -0,0 +1,15 @@
+package Page_Objects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SignUp {
+		
+		@FindBy(xpath="//*[@id=inputFirstName]")
+		public static WebElement FirstName;
+		
+		
+		
+	
+
+}
